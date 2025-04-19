aa57791a9f8670d38582695e0439b8e8eb7e2977
Danilo Borges Santos <borges_santos89@hotmail.com>
2025-04-19 20:36:23 -0300

    Implementações iniciais para criação de GraphicsDevice

--- src/xna/framework/graphics/graphics_device.rs
+++ src/xna/framework/graphics/graphics_device.rs
@@ -1,23 +1,36 @@
 use std::ptr;
 use std::boxed;
-use windows::core::Param;
-use windows::Win32::Foundation::HMODULE;
+use windows::core::{Param, BOOL};
+use windows::Win32::Foundation::{HMODULE, HWND};
 use windows::Win32::Graphics::Direct3D;
-use windows::Win32::Graphics::Direct3D11::{D3D11CreateDevice, D3D11CreateDeviceAndSwapChain, ID3D11Device, ID3D11DeviceContext, D3D11_CREATE_DEVICE_DEBUG, D3D11_CREATE_DEVICE_FLAG, D3D11_SDK_VERSION};
+use windows::Win32::Graphics::Direct3D11::{D3D11CreateDevice, D3D11CreateDeviceAndSwapChain, ID3D11BlendState, ID3D11DepthStencilState, ID3D11Device, ID3D11DeviceContext, ID3D11RasterizerState, ID3D11SamplerState, D3D11_BLEND, D3D11_BLEND_DESC, D3D11_BLEND_ONE, D3D11_BLEND_OP_ADD, D3D11_COMPARISON_LESS_EQUAL, D3D11_CREATE_DEVICE_DEBUG, D3D11_CREATE_DEVICE_FLAG, D3D11_CULL_BACK, D3D11_CULL_MODE, D3D11_DEPTH_STENCIL_DESC, D3D11_FILL_SOLID, D3D11_RASTERIZER_DESC, D3D11_RENDER_TARGET_BLEND_DESC, D3D11_SAMPLER_DESC, D3D11_SDK_VERSION, D3D11_VIEWPORT};
 use windows::Win32::Graphics::Direct3D::{D3D_DRIVER_TYPE, D3D_DRIVER_TYPE_HARDWARE, D3D_FEATURE_LEVEL, D3D_FEATURE_LEVEL_10_0, D3D_FEATURE_LEVEL_10_1, D3D_FEATURE_LEVEL_11_0, D3D_FEATURE_LEVEL_9_1, D3D_FEATURE_LEVEL_9_2, D3D_FEATURE_LEVEL_9_3};
-use windows::Win32::Graphics::Dxgi::{CreateDXGIFactory, IDXGIAdapter, IDXGIFactory, IDXGIFactory1};
-use crate::xna::framework::graphics::{GraphicsAdapter, GraphicsDevice};
+use windows::Win32::Graphics::Dxgi::{CreateDXGIFactory, IDXGIAdapter, IDXGIFactory, IDXGIFactory1, DXGI_MWA_FLAGS};
+use crate::xna::framework::Color;
+use crate::xna::framework::graphics::{GraphicsAdapter, GraphicsDevice, IPackedVector, PresentationParameters, SamplerState, SamplerStateCollection};
 
 pub struct WindowsGraphicsDevice {
     pub device: ID3D11Device,
     pub context: ID3D11DeviceContext,
     pub factory: IDXGIFactory,
     pub feature_level: D3D_FEATURE_LEVEL,
-    pub base: GraphicsDevice
+    pub background_color: Color,
+    pub parameters: WindowsPresentationParameters,
+    pub blend_state: ID3D11BlendState,
+    pub rasterizer_state: ID3D11RasterizerState,
+    pub depth_stencil_state: ID3D11DepthStencilState,
+    pub sampler_state_collection: Vec<Option<ID3D11SamplerState>>,
+    pub base: GraphicsDevice,
+}
+
+#[derive(Default, Eq, PartialEq, Copy, Clone)]
+pub struct WindowsPresentationParameters {
+    pub hwnd: HWND,
+    pub base: PresentationParameters,
 }
 
 impl GraphicsDevice {
-    pub fn create() -> WindowsGraphicsDevice{
+    pub fn create() -> WindowsGraphicsDevice {
         unsafe {
             let flags = D3D11_CREATE_DEVICE_DEBUG;
             let hmodule = HMODULE::default();
@@ -48,7 +61,7 @@ impl GraphicsDevice {
                 D3D11_SDK_VERSION,
                 Some(&mut device),
                 Some(&mut feature_level),
-                Some(&mut context)
+                Some(&mut context),
             ).unwrap();
 
             WindowsGraphicsDevice {
@@ -56,11 +69,155 @@ impl GraphicsDevice {
                 device: device.unwrap(),
                 factory: factory,
                 feature_level: feature_level,
+                background_color: Color::cornflower_blue(),
                 base: GraphicsDevice::default(),
+                ..Default::default()
             }
         }
     }
+}
 
-    pub fn initialize() {
+impl WindowsGraphicsDevice {
+    pub fn initialize(&mut self, parameters: &WindowsPresentationParameters) {
+        self.parameters = self.parameters;
+
+        unsafe {
+            //Window association
+            self.factory.MakeWindowAssociation(parameters.hwnd, DXGI_MWA_FLAGS::default())
+                .unwrap();
+
+            // Viewport
+            let viewport = [D3D11_VIEWPORT {
+                TopLeftX: 0.0,
+                TopLeftY: 0.0,
+                Width: parameters.base.back_buffer_width as f32,
+                Height: parameters.base.back_buffer_height as f32,
+                MaxDepth: 1.0,
+                MinDepth: 0.0,
+            }];
+
+            self.context.RSSetViewports(Some(&viewport));
+
+            // States
+            self.apply_blend_state();
+            self.apply_rasterizer_state();
+        }
+    }
+
+    fn apply_sampler_states(&mut self) {
+        let collection = &self.base.sampler_state_collection;
+
+        if collection.samplers.is_empty(){
+            return;
+        }
+
+        unsafe {
+            let mut samplers : Vec<Option<ID3D11SamplerState>> = Vec::new();
+
+            for i in 0..collection.samplers.len() {
+                let current = &collection.samplers[i];
+
+                let description = D3D11_SAMPLER_DESC::default();
+                let mut dx_sampler: Option<ID3D11SamplerState> = None;
+
+                //Initialize
+                self.device.CreateSamplerState(&description, Some(&mut dx_sampler)).unwrap();
+
+            }
+
+            //Apply
+            self.context.PSSetSamplers(0, Some(samplers.as_slice()));
+        }
+    }
+
+    fn apply_depth_stencil_state(&mut self) {
+        let depth_stencil = &self.base.depth_stencil_state;
+
+        //TODO implementar depth stencil state
+
+        let description = D3D11_DEPTH_STENCIL_DESC {
+            DepthFunc: D3D11_COMPARISON_LESS_EQUAL,
+            ..Default::default()
+        };
+
+        unsafe {
+            let mut dx_depth: Option<ID3D11DepthStencilState> = None;
+
+            // Initialize
+            self.device.CreateDepthStencilState(&description, Some(&mut dx_depth))
+                .unwrap();
+
+            // Apply
+            self.context.OMSetDepthStencilState(Some(&dx_depth), 0);
+
+            self.depth_stencil_state = dx_depth.unwrap();
+        }
+    }
+
+    fn apply_rasterizer_state(&mut self) {
+        let rasterizer = &self.base.rasterizer_state;
+
+        //TODO implementar rasterizer state
+
+        let description = D3D11_RASTERIZER_DESC {
+            CullMode: D3D11_CULL_BACK,
+            FillMode: D3D11_FILL_SOLID,
+            MultisampleEnable: BOOL(1),
+            DepthBias: 0,
+            SlopeScaledDepthBias: 0.0,
+            ScissorEnable: BOOL(0),
+            AntialiasedLineEnable: BOOL(1),
+            ..Default::default()
+        };
+
+        unsafe {
+            let mut dx_rasterizer: Option<ID3D11RasterizerState> = None;
+            // Initialize
+
+            self.device.CreateRasterizerState(&description, Some(&mut dx_rasterizer))
+                .unwrap();
+
+            // Apply
+            self.context.RSSetState(Some(&dx_rasterizer));
+
+            self.rasterizer_state = dx_rasterizer.unwrap();
+        }
+    }
+
+    fn apply_blend_state(&mut self) {
+        unsafe {
+            let blend_state = &self.base.blend_state;
+
+            let mut description = D3D11_BLEND_DESC::default();
+            description.AlphaToCoverageEnable = BOOL(blend_state.alpha_to_coverage_enable as i32);
+            description.AlphaToCoverageEnable = BOOL(1);
+
+            //TODO: implementar render targers
+
+            description.RenderTarget[0].BlendEnable = BOOL(1);
+            description.RenderTarget[0].SrcBlend = D3D11_BLEND_ONE;
+            description.RenderTarget[0].DestBlend = D3D11_BLEND_ONE;
+            description.RenderTarget[0].BlendOp = D3D11_BLEND_OP_ADD;
+            description.RenderTarget[0].SrcBlendAlpha = D3D11_BLEND_ONE;
+            description.RenderTarget[0].DestBlendAlpha = D3D11_BLEND_ONE;
+            description.RenderTarget[0].BlendOpAlpha = D3D11_BLEND_OP_ADD;
+            description.RenderTarget[0].RenderTargetWriteMask = 1;
+
+            let mut dx_blend_state: Option<ID3D11BlendState> = None;
+
+            // Initialize
+
+            self.device.CreateBlendState(&description, Some(&mut dx_blend_state))
+                .unwrap();
+
+            let factor = [1.0, 1.0, 1.0, 1.0];
+            let sample_mask = 0xffffffff;
+
+            // Apply
+
+            self.context.OMSetBlendState(Some(&dx_blend_state), Some(&factor), sample_mask);
+
+            self.blend_state = dx_blend_state.unwrap();
+        }
     }
 }
