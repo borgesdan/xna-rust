3f2ce4cf4a00a5f939f04266009a84750fa0b2cd
Danilo Borges Santos <borges_santos89@hotmail.com>
2025-04-22 10:27:40 -0300

    Adição de new em Blend State

--- src/xna/framework/graphics/blend_state.rs
+++ src/xna/framework/graphics/blend_state.rs
@@ -1,13 +1,32 @@
-use crate::xna::framework::graphics::{Blend, BlendState};
+use std::iter::Once;
+use crate::xna::framework::graphics::{Blend, BlendFunction, BlendRenderTarget, BlendState, ColorWriteChannels};
 
 impl BlendState {
+    pub fn new() -> BlendState {
+        BlendState {
+            alpha_to_coverage_enable: false,
+            independent_blend_enable: false,
+            render_targets: [BlendRenderTarget{
+                enabled: true,
+                source: Blend::One,
+                destination: Blend::One,
+                operation: BlendFunction::Add,
+                source_alpha: Blend::One,
+                destination_alpha: Blend::One,
+                operation_alpha: BlendFunction::Add,
+                write_mask: ColorWriteChannels::All,
+            };8],
+            ..Default::default()
+        }
+    }
+
     pub fn opaque() -> BlendState {
         BlendState {
             color_source_blend: Blend::One,
             alpha_source_blend: Blend::One,
             color_destination_blend: Blend::Zero,
             alpha_destination_blend: Blend::Zero,
-            ..Default::default()
+            ..Self::new()
         }
     }
 
@@ -17,7 +36,7 @@ impl BlendState {
             alpha_source_blend: Blend::One,
             color_destination_blend: Blend::InverseSourceAlpha,
             alpha_destination_blend: Blend::InverseSourceAlpha,
-            ..Default::default()
+            ..Self::new()
         }
     }
 
@@ -27,7 +46,7 @@ impl BlendState {
             alpha_source_blend: Blend::SourceAlpha,
             color_destination_blend: Blend::One,
             alpha_destination_blend: Blend::One,
-            ..Default::default()
+            ..Self::new()
         }
     }
 
@@ -37,7 +56,7 @@ impl BlendState {
             alpha_source_blend: Blend::SourceAlpha,
             color_destination_blend: Blend::InverseSourceAlpha,
             alpha_destination_blend: Blend::InverseSourceAlpha,
-            ..Default::default()
+            ..Self::new()
         }
     }
 }
