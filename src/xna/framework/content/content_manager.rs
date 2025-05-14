c0d3989fa946536ba1e4275c99ab2204fcfaaa16
Danilo Borges Santos <borges_santos89@hotmail.com>
2025-05-14 11:23:04 -0300

    Cria struct ContentManager

---
+++ src/xna/framework/content/content_manager.rs
@@ -0,0 +1,41 @@
+// use crate::xna::csharp::Exception;
+// use crate::xna::framework::content::{ContentManager, IContentManager};
+//
+// impl ContentManager{
+//     pub fn new(root_directory: &str)-> Self {
+//         ContentManager {
+//             root_directory: root_directory.to_string(),
+//         }
+//     }
+//
+//     fn read_asset<T>(&self, asset_name: &str) -> Result<Option<T>, Exception> {
+//
+//     }
+//
+//     fn open_stream(asset_name: &str) -> Result<Stre, Exception> {}
+// }
+//
+// impl IContentManager for ContentManager {
+//     fn get_root_directory(&self) -> &str {
+//         self.root_directory.as_str()
+//     }
+//
+//     fn set_root_directory(&mut self, value: &str) -> Result<(), Exception> {
+//         self.root_directory = value.to_string();
+//         Ok(())
+//     }
+//
+//     fn unload(&mut self) -> Result<(), Exception> {
+//         Ok(())
+//     }
+//
+//     fn load<T>(&mut self, asset_name: &str) -> Result<Option<T>, Exception> {
+//         if asset_name.is_empty() {
+//             return Err(Exception::argument_exception("Asset name is empty.", None));
+//         }
+//
+//         let obj3 = self.read_asset(asset_name)?;
+//
+//         Ok(obj3)
+//     }
+// }
